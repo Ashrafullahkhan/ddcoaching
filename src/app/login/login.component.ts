79f2ce0f6de44dfc2b7a77379850dfc671284416
Hemlata Ganuwala <63526238+YaseenKhamosh@users.noreply.github.com>
2022-11-15 14:29:05 +0000

    changed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  isSubmited = false;
+  logingForm = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+  constructor( public router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  get email(){
+    return this.logingForm.get("email")
+  }
+  get password(){
+    return this.logingForm.get('password')
+  }
+
+  submit(){
+    this.isSubmited = true;
+    if(!this.logingForm.valid){
+      return;
+    } 
+    const {email, password} = this.logingForm?.value;
   }
 
 }
